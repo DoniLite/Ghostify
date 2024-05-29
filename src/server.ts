cc54b0917f5f5833b1506bbd5b296267011c46a5
DoniLite <yaomessan13@outlokk.com>
2024-05-29 21:32:16 +0000

    maj

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,8 @@ import { Quote } from "./";
 import { index } from "./routes";
 import { WeatherData, EssentialWeatherData } from "./";
 import cors from "@fastify/cors";
+import { home } from "./routes/home";
+import { homeControler } from "./contoler/home";
 
 
 const server : FastifyInstance = fastify();
@@ -53,59 +55,8 @@ server.register(session, {
 });
 
 server.get("/", index);
-server.post("/home", async (req, res) => {
-
-  async function fetchRandomQuote(): Promise<Quote> {
-    const response = await fetch("https://api.quotable.io/random");
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data: Quote = await response.json();
-    console.log(`"${data.content}" —${data.author}`);
-    return data;
-  }
-  const quote = await fetchRandomQuote()
-  client.hSet("Quote", {
-    _id: quote._id,
-    content: quote.content,
-    author: quote.author,
-    authorSlug: quote.authorSlug,
-  });
-  
-
-
-  function extractEssentialWeatherData(
-    data: WeatherData
-  ): EssentialWeatherData {
-    const { datetime, tempmax, tempmin, conditions, description, icon } = data;
-    return { datetime, tempmax, tempmin, conditions, description, icon };
-  }
-  type BodyData = {
-    data?: {
-      country_capital: string;
-      country_flag: string;
-    };
-  };
-  const bodyData: BodyData = req.body;
-  const { data } = bodyData;
-  const userTown = data.country_capital;
-  const flag = data.country_flag;
-  console.log(data);
-  const WeatherResponse = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userTown}?unitGroup=metric&key=FLJ2SXSD4HVS6KL7Z6KFGCH8Y&contentType=json`
-  );
-  const weatherData = await WeatherResponse.json();
-  const weather: WeatherData = weatherData.days[0];
-  console.log(weatherData);
-  client.hSet("Weather", {...extractEssentialWeatherData(weather), flag: flag});
-  return res.send(JSON.stringify({ url: "/home" }));
-});
-server.get("/home", async (req, res) => {
-  const value = await client.hGetAll("Weather");
-  const quote = await client.hGetAll("Quote");
-  console.log(value);
-  return res.view("/src/views/index.ejs", { pagination: 1, activeIndex: 0, weatherData: value, quote: quote });
-});
+server.post("/home", homeControler);
+server.get("/home", home);
 
 server.get("/api/", index)
 
@@ -113,7 +64,7 @@ server.get('/blog', (req, res) => {
   return res.view("/src/views/blogOne.ejs", { pagination: 1, activeIndex: 3 });
 })
 
-const port = Number(process.env.PORT) || 3081;
+const port = parseInt(process.env.PORT) || 3081;
 server.listen({ port: port }, (err, address) => {
   if (err) {
     console.error(err);
