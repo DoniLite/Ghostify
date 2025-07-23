53315571f95c5838405ebafcf1ae48af7b34f291
DoniLite <yaomessan13@outlook.com>
2025-07-23 19:20:10 +0000

    chore: create document service table and define relations

--- src/db/schema/enums.ts
+++ src/db/schema/enums.ts
@@ -52,3 +52,8 @@ export const fundingProviderEnum = pgEnum('FundingProvider', [
 	'Wallet',
 	'Card',
 ]);
+export const revisionTypeEnum = pgEnum('RevisionType', [
+	'insert',
+	'format',
+	'delete',
+]);
--- src/lib/server/document/service/document.service.ts
+++ src/lib/server/document/service/document.service.ts
@@ -1,17 +1,17 @@
 import { BaseService } from '@/core/base.service';
+import { Service } from '@/core/decorators';
 import type { Document } from '@/db';
 import type { CreateDocumentDto, UpdateDocumentDto } from '../dto/document.dto';
 import { DocumentRepository } from '../repository/document.repository';
-import { Service } from '@/core/decorators';
 
 @Service()
 export class DocumentService extends BaseService<
-  Document,
-  CreateDocumentDto,
-  UpdateDocumentDto,
-  DocumentRepository
+	Document,
+	CreateDocumentDto,
+	UpdateDocumentDto,
+	DocumentRepository
 > {
-  constructor() {
-    super(new DocumentRepository());
-  }
+	constructor() {
+		super(new DocumentRepository());
+	}
 }
