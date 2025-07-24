f19e800033a09cd4d7234ab2df2d1c7c18b513f6
DoniLite <yaomessan13@outlook.com>
2025-07-24 16:39:06 +0000

    refactor: enhance getService in ServiceFactory with generic support

--- src/factory.ts
+++ src/factory.ts
@@ -11,9 +11,12 @@ export const factory = createFactory<{
 export class ServiceFactory {
 	private static instances = new Map();
 
-	static getService<T>(serviceClass: new () => T): T {
+	static getService<T, A>(
+		serviceClass: new (...args: A[]) => T,
+		...makers: A[]
+	): T {
 		if (!ServiceFactory.instances.has(serviceClass)) {
-			ServiceFactory.instances.set(serviceClass, new serviceClass());
+			ServiceFactory.instances.set(serviceClass, new serviceClass(...makers));
 		}
 		return ServiceFactory.instances.get(serviceClass);
 	}
