36318b676637fc0e6cb4bf5a937c77d7ee39f73d
DoniLite <yaomessan13@outlook.com>
2025-07-31 06:36:07 +0000

    fix: resolve some lint error and update the shad-cn lib export

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -2,7 +2,7 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { Menu } from 'lucide-react';
 import * as React from 'react';
-import { Button } from '@/components/ui/button';
+import type { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
 import {
